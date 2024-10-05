b96a04bcbaeff8e1bceeaae948888b238a6a1cbd
Busra2456 <you@example.com>
2024-10-05 16:50:16 +0600

    Job career hub

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Home from "../Home/Home";
+
+
+
+const Root = () => {
+      return (
+            <div className="max-w-6xl mx-auto" >
+               <div>
+               <Header></Header>
+               <Outlet></Outlet> 
+               </div>
+
+             <Footer></Footer>     
+            </div>
+      );
+};
+
+export default Root;
